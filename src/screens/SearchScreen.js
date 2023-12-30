1f19a5cec9fb763c3ad6ae883db14d47690ec725
Juan Feris <feris.juan@gmail.com>
2023-12-30 11:51:21 -0500

    custom hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,40 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 import yelp from '../api/yelp';
 
 import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
-    try {
-      setErrorMessage('');
-
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term,
-          location: 'san jose'
-        }
-      });
-
-      setResults(response.data.businesses);
-    }
-    catch (err) {
-      console.log(err);
-      setErrorMessage('Something went wrong');
-    }
-  }
+  const { results, errorMessage, searchApi } = useResults()
 
   return (
     <View>
       <SearchBar
         onTermChange={setTerm}
-        onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(term)}
         searchTerm={term}
       />
 
